dad262299e1f92b046f28a2288f5641f5312694b
Jonathan Gutiérrez <120731494+Goodtierrez@users.noreply.github.com>
2023-06-23 17:21:53 +0000

    add_member completed

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,6 +8,7 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -20,8 +21,19 @@ class FamilyStructure:
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # Tenemos que coger el objeto que nos pasa y complementar la información.
+        # El ID va a ser generado por el  _generateId()
+        # El last_name siempre tiene que ser el self.last_name
+
+        new_member = {
+            "id": self._generateId(),
+            "first_name": member['first_name'],
+            "last_name": self.last_name,
+            "age": member['age'],
+            "lucky_numbers": member['lucky_numbers']
+        }
+
+        self._members.append(new_member)
 
     def delete_member(self, id):
         # fill this method and update the return
